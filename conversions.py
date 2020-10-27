02b48ba1449faea6362044fb409b51b67e958283
LukeJSD <ljulian@mymail.mines.edu>
2020-10-27 00:54:30 -0600

    organize

--- conversions.py
+++
@@ -1,9 +0,0 @@
-def Time(t):
-    if type(t)==str:
-        t = t.split(':')
-        out = 0
-        out += float(t[0])*60
-        out += float(t[1])
-        return out
-    else:
-        return str(int(t//60))+':'+str(t%60)
